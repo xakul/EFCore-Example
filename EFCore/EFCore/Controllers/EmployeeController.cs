ac3c0ec46269a7bde31809a3c468262b99bf4eb5
Atakan Kul <40193937+xakul@users.noreply.github.com>
2021-04-02 23:29:14 +0300

    Scaffold with Northwind db and get and post operations in employee

---
+++ EFCore/EFCore/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using EFCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.Controllers
+{
+    [Route("api/employee")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AutoTempContext _context;
+
+        public EmployeeController(AutoTempContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet("get-employees")]
+        public async Task<ActionResult<Employee>> GetUsers()
+        {
+            return await _context.Employees.FirstOrDefaultAsync();
+        }
+    }
+}
